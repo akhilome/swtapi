e34da0dc3d27027a2342e9046cfb32e41266126f
Kizito Akhilome <kizito@akhilo.me>
2021-12-04 09:10:32 +0100

    setup helmet, api versioning, and validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
+
 import { AppModule } from './app.module';
 import { Environment } from './common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning();
+
+  app.use(helmet());
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
   const config = await app.get(ConfigService);
   await app.listen(config.get(Environment.PORT));
